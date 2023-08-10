94665f0acc1af5d6ac438c9b4496d7d33a058576
Petya <petya200606@gmail.com>
2023-08-11 00:31:34 +0600

    Сделал задание 15

--- task 15/Program.cs
+++ task 15/Program.cs
@@ -2,10 +2,15 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
-int number = new Random().Next(1, 7);
-Console.WriteLine($"Любое число диапазона от 1 до 7 -> {number}");
+int number = new Random().Next(1, 8);
+Console.WriteLine($"День недели от 1 до 7 -> {number}");
 //   Console.WriteLine("Введите номер от 1 до 7");
 //   string? number= Console.ReadLine();
- int firstDigit =
+if (number > 5)
+Console.WriteLine("Этот день выходной");
+else
+{
+    Console.WriteLine("Этот день не выходной");
+}
 
   
